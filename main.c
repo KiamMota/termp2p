8d863dd2845bc2a46bb90d7b09730eeebca4518d
KiamMota <somentekiam@gmail.com>
2025-06-01 23:30:32 -0300

    fix : functions in main.c now belong to init.h

--- main.c
+++ main.c
@@ -1,45 +1,14 @@
-#include <stdio.h>
-#include <string.h>
+#include "include/init.h"
 #include "include/user.h"
-#include "include/buffer.h"
-
-void menu();
-char get_choice();
-void get_nickname(user* usr);
+#include "include/text.h"
 
 int main()
 {
   user* usr = init_user();
-  char choice = 0;
   menu();
-  choice = get_choice();
   get_nickname(usr);
+  write(usr);
+  send(usr);
   return 0;
 }
 
-void menu()
-{
-  printf("welcome to termp2p \n");
-  printf("[1] - join in room \n");
-  printf("[2] - create a room\n");
-}
-
-char get_coice()
-{
-  char choice;
-  fgets(&choice, sizeof(choice), stdin);  
-  loop:
-    if(choice != 1 || choice != 2) 
-  goto loop;
-  
-  return choice;
-}
-
-void get_nickname(user* usr)
-{
-  char name[14];
-  printf("enter your nickname: ");
-  fgets(name, sizeof(name), stdin);
-  if(strcmp(name, "") == 0) return;
-  usr->nickame = name;
-}

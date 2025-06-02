b6f00c9bf7f78edf3e2e0432435c7bfab6f95f1a
KiamMota <somentekiam@gmail.com>
2025-06-01 22:52:18 -0300

    add : nickname, choice and menu

--- main.c
+++ main.c
@@ -1,9 +1,45 @@
 #include <stdio.h>
+#include <string.h>
+#include "include/user.h"
+#include "include/buffer.h"
+
+void menu();
+char get_choice();
+void get_nickname(user* usr);
 
 int main()
 {
-  printf("welcome to termp2p \n");
-    
-
+  user* usr = init_user();
+  char choice = 0;
+  menu();
+  choice = get_choice();
+  get_nickname(usr);
   return 0;
+}
+
+void menu()
+{
+  printf("welcome to termp2p \n");
+  printf("[1] - join in room \n");
+  printf("[2] - create a room\n");
+}
+
+char get_coice()
+{
+  char choice;
+  fgets(&choice, sizeof(choice), stdin);  
+  loop:
+    if(choice != 1 || choice != 2) 
+  goto loop;
+  
+  return choice;
+}
+
+void get_nickname(user* usr)
+{
+  char name[14];
+  printf("enter your nickname: ");
+  fgets(name, sizeof(name), stdin);
+  if(strcmp(name, "") == 0) return;
+  usr->nickame = name;
 }
